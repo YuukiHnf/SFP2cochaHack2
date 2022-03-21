b683a3407a7bc65fc877b89ae9775b48c3c1477b
YuukiHnf <hnf_yuuki@eis.hokudai.ac.jp>
2022-03-21 15:22:03 +0900

    Imageを表示させることに成功

--- components/admin/ObjectComponent.tsx
+++ components/admin/ObjectComponent.tsx
@@ -1,10 +1,13 @@
 import { VFC } from "react";
+import { url } from "../../utils/config";
 import DefaultGoogleMapComponent from "../googlemap/DefaultGoogleMapComponent";
 
 const ObjectComponent: VFC = () => {
   return (
     <>
       <DefaultGoogleMapComponent>{}</DefaultGoogleMapComponent>
+      <div>test</div>
+      <img src={url("/cornIcon.svg")} alt="" />
     </>
   );
 };
