6c74c20fd40e83a5bc581782355286df5b761281
YuukiHnf <hnf_yuuki@eis.hokudai.ac.jp>
2022-04-06 16:59:22 +0900

    表示を綺麗にして、Objectを配置した

--- components/googlemap/MapSettingComponent.tsx
+++ components/googlemap/MapSettingComponent.tsx
@@ -34,17 +34,21 @@ const rectAngleOption4 = {
   strokeOpacity: 0.3,
 };
 
+const rectAngleOption5 = {
+  fillColor: "darkred",
+  fillOpactiy: 0.3,
+  strokeColor: "darkred",
+  strokeOpacity: 0.3,
+};
+
 /**
  * 常に表示するObjectを表示
  */
 const MapSettingComponent = () => {
-  const [tapping, setTapping] = useState<
-    "Stage1" | "Stage2" | "Stage3" | "Souko4" | null
-  >(null);
   const [tappingIndex, setTappingIndex] = useState<number>(-1);
   const setObjects = useAppSelector(selectSetObjects);
   const { deleteSetObject } = useSetObjectHooks();
-  //console.log(setObjects);
+  console.log(tappingIndex);
   return (
     <>
       {setObjects &&
@@ -56,7 +60,13 @@ const MapSettingComponent = () => {
                   <Polygon
                     key={setObj.id}
                     path={setObj.locations}
-                    options={rectAngleOption3}
+                    options={
+                      setObj.desc.endsWith("ステージ")
+                        ? rectAngleOption
+                        : setObj.desc.endsWith("テント")
+                        ? rectAngleOption4
+                        : rectAngleOption3
+                    }
                     onClick={() => {
                       setTappingIndex(index);
                     }}
@@ -80,7 +90,7 @@ const MapSettingComponent = () => {
                   key={setObj.id}
                   center={setObj.locations[0]}
                   radius={setObj.locations[1].lat}
-                  options={rectAngleOption4}
+                  options={rectAngleOption5}
                   onClick={() => {
                     setTappingIndex(index);
                   }}
@@ -105,86 +115,6 @@ const MapSettingComponent = () => {
           </>
         </InfoWindow>
       )}
-      {/* <Polygon
-        path={[
-          new google.maps.LatLng(43.080180692594475, 141.34037284277449),
-          new google.maps.LatLng(43.07991425690792, 141.34044258020887),
-          new google.maps.LatLng(43.07994560234294, 141.34059814833174),
-          new google.maps.LatLng(43.08020666961666, 141.3405176820613),
-        ]}
-        options={rectAngleOption}
-        onClick={() => {
-          setTapping("Stage1");
-        }}
-      /> */}
-      {/* {tapping === "Stage1" && (
-        <InfoWindow position={new google.maps.LatLng(43.0802, 141.34045)}>
-          <div>特設ステージ</div>
-        </InfoWindow>
-      )}
-      <Polygon
-        path={[
-          new google.maps.LatLng(43.0804520248051, 141.34076490132765),
-          new google.maps.LatLng(43.08056056624953, 141.34078953690962),
-          new google.maps.LatLng(43.08058513929558, 141.34100093572096),
-          new google.maps.LatLng(43.08048043135694, 141.34108944861845),
-        ]}
-        options={rectAngleOption2}
-        onClick={() => {
-          setTapping("Stage2");
-        }}
-      />
-      {tapping === "Stage2" && (
-        <InfoWindow
-          position={
-            new google.maps.LatLng(43.080593077898364, 141.34089096515135)
-          }
-        >
-          <div>特設ステージ2</div>
-        </InfoWindow>
-      )}
-      <Polygon
-        path={[
-          new google.maps.LatLng(43.080280605678226, 141.3396075386151),
-          new google.maps.LatLng(43.080195433795495, 141.3396365507385),
-          new google.maps.LatLng(43.080239465016405, 141.339986544303),
-          new google.maps.LatLng(43.080325145143576, 141.33997157158686),
-        ]}
-        options={rectAngleOption3}
-        onClick={() => {
-          setTapping("Stage3");
-        }}
-      />
-      {tapping === "Stage3" && (
-        <InfoWindow
-          position={
-            new google.maps.LatLng(43.08028541379676, 141.33959455686127)
-          }
-        >
-          <div>演者待機場所</div>
-        </InfoWindow>
-      )}
-      <Polygon
-        path={[
-          new google.maps.LatLng(43.08034329615737, 141.34010956194692),
-          new google.maps.LatLng(43.08030803277074, 141.34011760857396),
-          new google.maps.LatLng(43.08032008806707, 141.34019818358428),
-          new google.maps.LatLng(43.080355705899384, 141.34019584247977),
-        ]}
-        options={rectAngleOption4}
-        onClick={() => {
-          setTapping("Souko4");
-        }}
-      />
-      {tapping === "Souko4" && (
-        <InfoWindow
-          position={
-            new google.maps.LatLng(43.080355705899384, 141.34019584247977)
-          }
-        >
-          <div>物品置き場</div>
-        </InfoWindow>
-      )} */}
     </>
   );
 };
